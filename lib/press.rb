28354ed827cd03c61d544c6edee95d9059587a8b
Mark Fine <mark.fine@gmail.com>
2012-08-29 10:50:19 -0700

    Add PressLogger to base class.

--- lib/press.rb
+++ lib/press.rb
@@ -22,3 +22,18 @@ module Press
     Printer.pdfme file, m, e, *data
   end
 end
+
+class PressLogger
+
+  def self.level(*attrs)
+    attrs.each do |attr|
+      class_eval "def #{attr} args; Printer.pd type: @type, level: '#{attr}', message: args end", __FILE__, __LINE__
+    end
+  end
+
+  level :debug, :info, :warn, :error, :fatal
+
+  def initialize(type)
+    @type = type
+  end
+end
