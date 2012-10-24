290eff2e9a3e5a268e8916463712960d46372342
Mark Fine <mark.fine@gmail.com>
2012-10-23 23:57:17 -0700

    inspect up the array.

--- lib/press/printer.rb
+++ lib/press/printer.rb
@@ -20,11 +20,11 @@ module Press
     end
 
     def self.pde(e, *data)
-      write $stderr, hashify(*data, :at => "error", :class => e.class, :message => e.message.lines.to_a[0], :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5])
+      write $stderr, hashify(*data, :at => "error", :class => e.class, :message => e.message.lines.to_a.first, :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5].inspect)
     end
 
     def self.pdfme(file, m, e, *data)
-      write $stderr, hashify(*data, :at => "error", :class => e.class, :message => e.message.lines.to_a[0], :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5], :file => File.basename(file, ".rb"), :fn => m)
+      write $stderr, hashify(*data, :at => "error", :class => e.class, :message => e.message.lines.to_a.first, :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5].inspect, :file => File.basename(file, ".rb"), :fn => m)
     end
 
     def self.hashify(*data, initial)
