2db46606cdfcf5b8aa3b09f48089c3437cd87823
Mark Fine <mark.fine@gmail.com>
2012-11-13 22:25:40 -0800

    add in measure calls.

--- lib/press.rb
+++ lib/press.rb
@@ -6,20 +6,40 @@ module Press
     Printer.ctx = data
   end
 
+  def mtx(tag)
+    Printer.mtx = tag
+  end
+
   def pd(*data, &blk)
     Printer.pd *data, &blk
+  end
+
+  def mpd(*data, &blk)
+    Printer.mpd *data, &blk
   end
 
   def pdfm(file, m, *data, &blk)
     Printer.pdfm file, m, *data, &blk
   end
 
+  def mpdfm(file, m, *data, &blk)
+    Printer.mpdfm file, m, *data, &blk
+  end
+
   def pde(e, *data)
     Printer.pde e, *data
   end
 
+  def mpde(e, *data)
+    Printer.mpde e, *data
+  end
+
   def pdfme(file, m, e, *data)
     Printer.pdfme file, m, e, *data
+  end
+
+  def mpdfme(file, m, e, *data)
+    Printer.mpdfme file, m, e, *data
   end
 
   class Logger
--- lib/press/printer.rb
+++ lib/press/printer.rb
@@ -11,24 +11,48 @@ module Press
       @ctx = data
     end
 
+    def self.mtx=(tag)
+      @mtx = tag
+    end
+
     def self.pd(*data, &blk)
       write $stdout, hashify(*data, {}), &blk
+    end
+
+    def self.mpd(*data, &blk)
+      write $stdout, mhashify(@mtx, *data, {}), &blk
     end
 
     def self.pdfm(file, m, *data, &blk)
       write $stdout, hashify(*data, :file => File.basename(file, ".rb"), :fn => m), &blk
     end
 
+    def self.mpdfm(file, m, *data, &blk)
+      write $stdout, mhashify([@mtx, File.basename(file, ".rb"), m].compact.join("."), *data, :file => File.basename(file, ".rb"), :fn => m), &blk
+    end
+
     def self.pde(e, *data)
       write $stderr, hashify(*data, :at => "error", :class => e.class, :message => e.message.lines.to_a.first, :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5].inspect)
+    end
+
+    def self.mpde(e, *data)
+      write $stderr, mhashify(@mtx, *data, :at => "error", :class => e.class, :message => e.message.lines.to_a.first, :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5].inspect)
     end
 
     def self.pdfme(file, m, e, *data)
       write $stderr, hashify(*data, :at => "error", :class => e.class, :message => e.message.lines.to_a.first, :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5].inspect, :file => File.basename(file, ".rb"), :fn => m)
     end
 
+    def self.mpdfme(file, m, e, *data)
+      write $stderr, mhashify([@mtx, File.basename(file, ".rb"), m].compact.join("."), *data, :at => "error", :class => e.class, :message => e.message.lines.to_a.first, :trace => e.backtrace.map { |i| i.match(/(#{Gem.dir}|#{Dir.getwd})?\/(.*):in (.*)/) && $2 }[0..5].inspect, :file => File.basename(file, ".rb"), :fn => m)
+    end
+
     def self.hashify(*data, initial)
       data.compact.reduce(initial.merge(ctx)) { |d, v| d.merge v }
+    end
+
+    def self.mhashify(mtx, *data, initial)
+      hashify(*data, initial).tap { |d| d[:measure] = [mtx, d[:measure]].compact.join(".") if mtx }
     end
 
     def self.stringify(data)
